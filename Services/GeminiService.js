5266b17209874de60beb8a0caaa9fe147e7f42fd
gustavoal20 <gustavoalbarracin84@gmail.com>
2026-08-06 23:49:51 -0300

    version 2 de geminis

--- Services/GeminiService.js
+++ Services/GeminiService.js
@@ -117,7 +117,7 @@ export default class GeminiService {
     static async processChat(messages) {
         // En Vertex AI instanciamos el modelo usando 'gemini-1.5-flash'
         const generativeModel = vertexAI.getGenerativeModel({
-            model: 'gemini-1.5-flash-001',
+            model: 'gemini-1.5-flash-002',
             systemInstruction: {
                 parts: [{ text: SYSTEM_PROMPT }]
             },
